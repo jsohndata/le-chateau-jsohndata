73398a4299f72f423d3ddeabd26f65ea7d605ee8
Jiho Sohn <jsohndata@gmail.com>
2024-07-04 22:55:52 -0400

    UPDATE footer with github image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return(
         <footer>
             <p><a href="https://github.com/jsohndata/le-chateau-jsohndata" target="_blank" rel="noreferrer">
-                <img src="https://jsohndata.s3.amazonaws.com/images/general/github.svg" alt="GitHub" />See code in GitHub</a></p>
+                <img src="https://luminkai.com/images/icons/social/circle/github.svg" alt="GitHub" />See code in GitHub</a></p>
         </footer>
     )
 }
